554b86056ea73975c8eb93c9943736355d946d7e
nirakar <lmntrix.as619@gmail.com>
2024-03-20 17:36:23 +0545

    changed login screen

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect, useLayoutEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, SafeAreaView, TouchableOpacity } from "react-native";
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    TouchableOpacity,
+    ScrollView,
+    Platform,
+} from "react-native";
 import { Button, Input } from "react-native-elements";
 import { AntDesign } from "@expo/vector-icons";
-import { auth } from "../../../firebase";
+import { auth, db } from "../../firebase";
+import firebase from "firebase";
 import PropTypes from "prop-types";
+import CustomGoogleLoginButton from "../../custom/loginButtons/android/Google";
 
-export default function LoginScreenEmail({ navigation }) {
+export default function LoginScreen({ navigation }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     useEffect(() => {
@@ -15,14 +25,28 @@ export default function LoginScreenEmail({ navigation }) {
         });
         return unSubscribe;
     }, []);
-    const signIn = () => {
-        auth.signInWithEmailAndPassword(email, password).catch((error) =>
-            alert(error.message)
-        );
+    const signInEmail = () => {
+        auth.signInWithEmailAndPassword(email, password)
+            .then(() => {
+                db.collection("publicNotifications").add({
+                    title: "Member came back to the Ligtning Family!!",
+                    message: `${email} came back to the Ligtning Family!! Yippie!!`,
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                });
+            })
+            .then(() => {
+                db.collection("privateNotifications").add({
+                    title: "Welcome Back!!",
+                    message: `Welcome back ${email}. Nice to meet you again!!`,
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                    user: email,
+                });
+            })
+            .catch((error) => alert(error.message));
     };
     useLayoutEffect(() => {
         navigation.setOptions({
-            title: "Login with Email!!",
+            title: "Login!!",
             headerLeft: () => (
                 <SafeAreaView style={{ flex: 1 }}>
                     <TouchableOpacity
@@ -55,11 +79,11 @@ export default function LoginScreenEmail({ navigation }) {
                     type="password"
                     value={password}
                     onChangeText={(text) => setPassword(text)}
-                    onSubmitEditing={signIn}
+                    onSubmitEditing={signInEmail}
                 />
             </View>
             <Button
-                onPress={signIn}
+                onPress={signInEmail}
                 containerStyle={styles.button}
                 title="Login"
             />
@@ -67,14 +91,60 @@ export default function LoginScreenEmail({ navigation }) {
                 containerStyle={styles.button}
                 title="Register"
                 type="outline"
-                onPress={() => navigation.navigate("RegisterWithEmail")}
+                onPress={() => navigation.navigate("Register")}
             />
-            <View style={{ height: 100 }} />
+
+            {Platform.OS === "android" && (
+                <View>
+                    <Text
+                        style={{
+                            marginTop: 10,
+                            marginBottom: 10,
+                            color: "#594d4c",
+                            alignSelf: "center",
+                            fontSize: 20,
+                        }}
+                    >
+                        Or
+                    </Text>
+                    <ScrollView
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                        }}
+                    >
+                        <CustomGoogleLoginButton />
+                    </ScrollView>
+                </View>
+            )}
+            {Platform.OS === "ios" && (
+                <View>
+                    <Text
+                        style={{
+                            marginTop: 10,
+                            marginBottom: 10,
+                            color: "#594d4c",
+                            alignSelf: "center",
+                            fontSize: 20,
+                        }}
+                    >
+                        Or
+                    </Text>
+                    <ScrollView
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                        }}
+                    >
+                        <CustomAppleLoginButton />
+                    </ScrollView>
+                </View>
+            )}
         </View>
     );
 }
 
-LoginScreenEmail.propTypes = {
+LoginScreen.propTypes = {
     navigation: PropTypes.object.isRequired,
 };
 
@@ -82,15 +152,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center",
         padding: 10,
         backgroundColor: "white",
     },
     inputContainer: {
         width: 300,
+        marginTop: 10,
     },
     button: {
         width: 200,
         marginTop: 10,
+    },
+    loginButton: {
+        width: 300,
+        marginTop: 40,
     },
 });
